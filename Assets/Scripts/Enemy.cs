bf107a6964d9b138799d28c88fde42b1c6bfc3f6
Chris Janidlo <christopherjanidlo@gmail.com>
2022-12-11 16:33:09 -0600

    Enemy death animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,14 +12,26 @@ public class Enemy : MonoBehaviour
     [Header("References")] [SerializeField]
     private new Rigidbody2D rigidbody2D;
 
+    [SerializeField] private new Collider2D collider;
+
     [SerializeField] private Reflection reflection;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private string deathAnimationStateName;
+    [SerializeField] private Material deathMaterial;
+
     [SerializeField] private Vector2Variable playerPosition;
     [SerializeField] private IntVariable killCount;
+
+    private bool _dying;
 
     public float ReflectionRadius => reflection.Radius;
 
     private void FixedUpdate()
     {
+        if (_dying) return;
+
         rigidbody2D.velocity = (playerPosition.Value - rigidbody2D.position).normalized * followSpeed;
     }
 
@@ -41,10 +54,24 @@ public class Enemy : MonoBehaviour
 
     public void Kill()
     {
-        Debug.Log("ahh I'm dying");
-        Destroy(gameObject);
+        IEnumerator DeathRoutine()
+        {
+            rigidbody2D.simulated = false;
+            collider.enabled = false;
 
-        killCount.Value++;
-        onDeath.Invoke();
+            animator.Play(deathAnimationStateName);
+            spriteRenderer.material = deathMaterial;
+
+            // TODO: this is super gross, there has to be a better way
+            yield return null;
+            yield return new WaitWhile(() => animator.GetCurrentAnimatorStateInfo(0).IsName(deathAnimationStateName));
+
+            Destroy(gameObject);
+
+            killCount.Value++;
+            onDeath.Invoke();
+        }
+
+        StartCoroutine(DeathRoutine());
     }
 }
